ffd56521f31185a6ab12c292b0c57e44ac4b0769
MutatedGamer <lukebordonaro@lukes-mbp.watson.ibm.com>
2016-07-29 14:55:56 -0400

    py

--- static/test.py
+++ static/test.py
@@ -1,1 +1,14 @@
-print("<html><head>Test</head></html>")
+import sys, json
+
+# Load the data that PHP sent us
+try:
+    data = json.loads(sys.argv[1])
+except:
+    print "ERROR"
+    sys.exit(1)
+
+# Generate some data to send to PHP
+result = {'status': 'Yes!'}
+
+# Send it to stdout (to PHP)
+print json.dumps(result)
